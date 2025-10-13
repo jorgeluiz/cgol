91d891599f3b7df1842e1f5882357283ffae5f36
Jorge Junior <luizsilvajj@gmail.com>
2025-10-12 22:57:45 -0300

    jump game

--- dl-game-of-life-frontend/src/components/page-header.tsx
+++ dl-game-of-life-frontend/src/components/page-header.tsx
@@ -1,14 +1,26 @@
 "use client"
+
+import { useState } from "react";
 import { useGameStore } from "@/stores/game-store"
 
 import { useGameActions } from "@/hooks/game-hooks";
 
 export default function Header() {
-    const { newGame, finishGame, nextStage, startAutoPlay, stopAutoPlay } = useGameActions();
+    const { newGame, finishGame, nextState, startAutoPlay, stopAutoPlay, fastForward } = useGameActions();
 
+    const [stateIncrement, setStateIncrement] = useState('1');
     const board = useGameStore((state) => state.board);
     const isBoardLocked = useGameStore((state) => state.isBoardLocked);
     const isAutoPlaying = useGameStore((state) => state.isAutoPlaying);
+
+    const handleStateIncrement = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setStateIncrement(evt.target.value);
+    };
+
+    const handleIncrement = () => {
+        const forwardCount = parseInt(stateIncrement) || 1;
+        fastForward(forwardCount);
+    }
 
     return <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -28,18 +40,34 @@ export default function Header() {
                         board?.id != null && (
                             <>
                                 <li>
-                                    <button className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" onClick={nextStage} disabled={isBoardLocked || isAutoPlaying}>Next state</button>
+                                    <div className="flex items-center space-x-2">
+                                        <input
+                                            type="number"
+                                            min="0"
+                                            value={stateIncrement || undefined}
+                                            className="w-16 text-center border border-gray-300 rounded py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            onChange={handleStateIncrement}
+                                        />
+                                        <button
+                                            className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                            onClick={handleIncrement}
+                                        >
+                                            Jump states
+                                        </button>
+                                    </div>
                                 </li>
                                 <li>
-                                    <button className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" disabled={isBoardLocked} onClick={isAutoPlaying ? stopAutoPlay : startAutoPlay}> {isAutoPlaying ? 'Stop Auto' : 'Start Auto'}</button>
+                                    <button className="mt-2 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" onClick={nextState} disabled={isBoardLocked || isAutoPlaying}>Next state</button>
                                 </li>
                                 <li>
-                                    <button className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" onClick={finishGame} disabled={isBoardLocked || isAutoPlaying}>Clear board</button>
+                                    <button className="mt-2 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" disabled={isBoardLocked} onClick={isAutoPlaying ? stopAutoPlay : startAutoPlay}> {isAutoPlaying ? 'Stop Auto' : 'Start Auto'}</button>
+                                </li>
+                                <li>
+                                    <button className="mt-2 block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" onClick={finishGame} disabled={isBoardLocked || isAutoPlaying}>Clear board</button>
                                 </li>
                             </>
                         )
                     }
-
                 </ul>
             </div>
         </div>
